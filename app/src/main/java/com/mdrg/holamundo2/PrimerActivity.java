1578031f8ef762f362ca126e525bbbaac2672697
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-11-06 13:21:39 -0500

    inicial

---
+++ app/src/main/java/com/mdrg/holamundo2/PrimerActivity.java
@@ -0,0 +1,14 @@
+package com.mdrg.holamundo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer);
+    }
+}
